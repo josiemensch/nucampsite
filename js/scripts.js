044fb1b4657d5104b6af42876b2f6fce9e8335d4
Josephine Mensching <josiemensching72@gmail.com>
2022-03-12 11:27:22 -0500

    week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,10 @@
                 }
             });
 
+            $("#reserveButton").click(function() {
+                $("#reserveModal").modal(`show`);
+            });
+            $("#loginButton").click(function() {
+                $("#loginModal").modal(`show`);
+            });    
         });
